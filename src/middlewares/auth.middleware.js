aa99cc23b0c07ef6ffc72cd958f4bf39aaa79478
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2024-06-29 20:47:07 +0530

    add console to solve error

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,7 +6,7 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 export const varifyJwt = asyncHandler(async (req, _, next) => {
    try {
     const token =  req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-   //  console.log(token);
+    console.log(token);
     if(!token) throw new ApiError(401," unauthorized request");
  
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
